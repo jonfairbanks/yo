7f15c510adfc2b32c811f1f8d3c2502127336fb0
Jon Fairbanks <fairplay89@gmail.com>
2019-04-24 21:28:06 -0700

    Initial Docker Setup

--- server/config/config.js
+++ server/config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  mongoURI: "mongodb://yo:qqq123@localhost:27018/yo",
+  mongoURI: "mongodb://localhost/yo",
   errorUrl: "https://yo.fairbanks.io/error",
   baseUrl: "https://yo.fairbanks.io"
 };
