a4b0c7f66816d84b6f143ea0f115a898b378f3b4
Jon Fairbanks <fairplay89@gmail.com>
2019-04-24 22:06:03 -0700

    Rename Files

--- server/config/config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  mongoURI: "mongodb://localhost/yo",
-  errorUrl: "https://yo.fairbanks.io/error",
-  baseUrl: "https://yo.fairbanks.io"
-};
--- server/config/redis.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  redisUrl: 'redis://127.0.0.1:6379',
-};
