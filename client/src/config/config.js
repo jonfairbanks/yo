912901226f24356c5ada487bf67466aa4a9246d8
Jon Fairbanks <fairplay89@gmail.com>
2019-05-17 17:09:57 -0700

    Update Configs

--- client/src/config/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  apiUrl: "https://yo-api.fairbanks.io/api/",
-  socketUrl: "https://yo-api.fairbanks.io",
-  baseUrl: "https://yo.fairbanks.io",
-  urlPlaceholder: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
-  namePlaceholder: "Rick"
-};
