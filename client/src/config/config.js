b5705572f81558e4e545cb3d68fb2d93dfaecf27
BSord <brandon.sorgdrager@gmail.com>
2019-05-19 22:19:01 +0000

    remove config

--- client/src/config/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  apiUrl: "https://id-api.bsord.io/api/",
-  socketUrl: "https://id-api.bsord.io",
-  baseUrl: "https://id.bsord.io",
-  urlPlaceholder: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
-  namePlaceholder: "Rick"
-};
