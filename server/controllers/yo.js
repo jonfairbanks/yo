af99318fb71610e4196a2a61227b111e2d201b84
BSord <brandon.sorgdrager@gmail.com>
2019-05-20 00:38:24 +0000

    Rework routes and remove caching

--- server/controllers/yo.js
+++ server/controllers/yo.js
@@ -1,61 +1,161 @@
 const mongoose = require('mongoose');
 mongoose.promise = Promise;
-const redis = require('redis');
-const keys = require('../config/redis');
 const Yo = mongoose.model('yo');
 const logger = require('../services/logger');
 const config = require('../config/config');
-const redisClient = redis.createClient(keys.redisUrl);
+const validUrl = require('valid-url');
 
 
-// GET YO FROM DB
-exports.get = (req, res) => {
+// Get a single Yo from DB
+exports.getYo = (req, res) => {
     const ip = req.headers["x-real-ip"];
     const linkName = req.params.name.toLowerCase();
     
     Yo.findOneAndUpdate({ linkName: linkName }, {$inc : {urlHits : 1}, $set:{lastAccess:Date.now()}})
-        .catch( error => {
-            logger.warn("Unable to find any entries for: " + linkName + '; ' + error);
-            return res.redirect(config.errorUrl);
-        })
         .then( item => {
-            logger.info("User from " + ip + " loaded " + item.originalUrl + " as alias: " + linkName);
-            return res.redirect(item.originalUrl);
+            if(item){ // item returned is not empty
+                logger.info("User from " + ip + " loaded " + item.originalUrl + " as alias: " + linkName);
+                return res.redirect(item.originalUrl);
+            } else { //item returned is empty
+                logger.warn("Unable to find any entries for: " + linkName);
+                return res.redirect(config.errorUrl);
+            }
+ 
+        }).catch( error => {
+            logger.warn("There was an error while searching database for: " + linkName + '; ' + error);
+            return res.redirect(config.errorUrl);
         });
-
 }
 
-// GET YO FROM CACHE
-exports.getCached = (req, res) => {
+// Add new Yo to DB
+exports.postYo = (req, res) => {
+    const { shortBaseUrl, originalUrl, linkName } = req.body;
     const ip = req.headers["x-real-ip"];
-    const linkName = req.params.name.toLowerCase();
 
-    redisClient.hget('linkName', JSON.stringify({linkName}), (error, item)=> {
-        if(error){ //there was an error in redis
-            logger.warn("There was an error with cache while trying to find " + linkName + '; ' + error);
-            return res.redirect(config.errorUrl);
-        }
-        else if (item) { //Item exists in cache, redirect using item from cache
-            logger.info("User from " + ip + " loaded " + item.originalUrl + " as alias: " + linkName + " from cache");
-            return res.redirect(JSON.parse(item).originalUrl);
-        }
-        else { //Item does not exist in cache, pull from the database
-            Yo.findOne({ linkName: linkName })
-                .catch( error => {
-                    logger.warn("There was an error while searching database for: " + linkName + '; ' + error);
-                    return res.redirect(config.errorUrl);
-                })
-                .then( item => {
-                    if(item){ // item returned is not empty
-                        logger.info("User from " + ip + " loaded " + item.originalUrl + " as alias: " + linkName);
-                        return res.redirect(item.originalUrl);
-                    } else { //item returned is empty
-                        logger.warn("Unable to find any entries for: " + linkName + '; ' + error);
-                        return res.redirect(config.errorUrl);
-                    }
-                    
+    const updatedAt = new Date();
+    const queryOptions = { linkName };
+
+    if(validUrl.isUri(originalUrl)) {
+
+        Yo.findOne(queryOptions)
+            .then( urlData => {
+                
+                if(urlData) {
+                    // URL already exists
+                    logger.info("User " + ip + " could not create a Yo as the name is already in-use: " + queryOptions.linkName);
+                    res.status(401).json('This name is already in-use. Please select another name.');
+
+                } else {
+                    // Define Yo object
+                    shortUrl = shortBaseUrl + '/' + linkName;
+                    const itemToBeSaved = { originalUrl, shortUrl, linkName, updatedAt };
+        
+                    // Add the item to db
+                    const item = new Yo(itemToBeSaved);
+
+                    item.save().then(()=>{
+                        logger.info("User from " + ip + " created alias: " + linkName + " -> " + originalUrl);
+                        res.status(200).json(itemToBeSaved);
+
+                    }).catch(error=>{
+                        logger.info("Error while trying to save Yo:" + linkName + " -> " + originalUrl + " to database: " + error);
+                        itemToBeSaved.status = "Failed"
+                        res.status(200).json(itemToBeSaved);
+                    });
+
+                }
+            }).catch( error => {
+                //Handle Error
+            });
+
+    } else {
+      logger.warn("The provided URL is improperly formatted: " + originalUrl);
+      return res.status(400).json('The provided URL is improperly formatted.');
+    }
+}
+
+// Get Yo statistics
+exports.getStats = (req, res) => {
+    var hits = 0;
+    Yo.find({},{urlHits:1, _id:0}).sort({urlHits: -1})
+        .then( hits_data => {
+            for(i = 0; i < hits_data.length; i++) { 
+                if(hits_data[i].urlHits){
+                hits += hits_data[i].urlHits;
+                }
+            }
+            if(hits_data.length > 0) {
+                return res.status(200).json({
+                "totalYos": hits_data.length,
+                "totalHits": hits
                 });
-        }
-    });
+            }else {
+                logger.error("Error retrieving Yo stats: " + res.data);
+                return res.status(500).json('Error retrieving Yo stats');
+            }
+        }).catch( error => {
+            //Handle Error
+        });
+    
+}
+
+// Get recent Yos
+exports.getRecent = (req, res) => {
+    Yo.find({}).sort({"lastAccess": -1}).limit(10)
+        .then(rec => {
+            if(rec) {
+                return res.status(200).json(rec);
+            }else {
+                logger.error("Error retrieving recently used Yo\'s: " + res.data);
+                return res.status(500).json('Error retrieving recently used Yo\'s');
+            }
+        }).catch( error => {
+            //Handle Error
+        });
+    
+}
+
+exports.getPopular = (req, res) => {
+    Yo.find({}).sort({"urlHits": -1}).limit(10)
+        .then(pop=>{
+            if(pop) {
+                return res.status(200).json(pop);
+            }else {
+                logger.error("Error retrieving popular Yo\'s: " + res.data);
+                return res.status(500).json('Error retrieving popular Yo\'s');
+            }
+        }).catch( error => {
+            //Handle Error
+        });
         
 }
+
+exports.getLatest = (req, res) => {
+    Yo.find({}).sort({"createdAt": -1}).limit(10)
+        .then(latest=>{
+            if(latest) {
+                return res.status(200).json(latest);
+            }else {
+                logger.error("Error retrieving the latest Yo\'s: " + res.data);
+                return res.status(500).json('Error retrieving the latest Yo\'s');
+            }
+        }).catch( error => {
+            //Handle Error
+        });
+    
+}
+
+exports.getAll = (req, res) => {
+    Yo.find({}).sort({"linkName": 1})
+        .then(all => {
+            if(all) {
+                return res.status(200).json(all);
+            }else {
+                logger.error("Error retrieving all Yo\'s: " + res.data);
+                return res.status(500).json('Error retrieving all Yo\'s');
+            }
+        }).catch( error => {
+            //Handle Error
+        });
+    
+  }
--- server/routes/yo.js
+++ server/routes/yo.js
@@ -1,123 +1,17 @@
-const mongoose = require('mongoose');
-const validUrl = require('valid-url');
-const Yo = mongoose.model('yo');
 const YoCtrl = require('../controllers/yo')
 const config = require('../config/config');
-const logger = require('../services/logger');
-const cache = require('../services/cache');
 
 module.exports = app => {
-  app.get('/', async (req, res) => {
+  app.get('/', (req, res) => {
     res.redirect(config.baseUrl);
   });
   
-  app.get('/api/', async (req, res) => {
-    const all = await Yo.find({}).sort({"linkName": 1});
-    if(all) {
-      return res.status(200).json(all);
-    }else {
-      logger.error("Error retrieving all Yo\'s: " + res.data);
-      return res.status(500).json('Error retrieving all Yo\'s');
-    }
-  });
+  app.get('/api/', YoCtrl.getAll) // Get all Yos
+  app.get('/api/latest', YoCtrl.getLatest) // Get latest Yos
+  app.get('/api/popular', YoCtrl.getPopular) // Get popular Yos
+  app.get('/api/recent', YoCtrl.getRecent) // Get recent Yos
+  app.get('/api/item/:name', YoCtrl.getYo) // Get a single Yo
+  app.post('/api/item', YoCtrl.postYo) // Post a single Yo
+  app.get('/api/stats', YoCtrl.getStats) // Get statistics
 
-  app.get('/api/latest', async (req, res) => {
-    const latest = await Yo.find({}).sort({"createdAt": -1}).limit(10);
-    if(latest) {
-      return res.status(200).json(latest);
-    }else {
-      logger.error("Error retrieving the latest Yo\'s: " + res.data);
-      return res.status(500).json('Error retrieving the latest Yo\'s');
-    }
-  });
-
-  app.get('/api/popular', async (req, res) => {
-    const pop = await Yo.find({}).sort({"urlHits": -1}).limit(10);
-    if(pop) {
-      return res.status(200).json(pop);
-    }else {
-      logger.error("Error retrieving popular Yo\'s: " + res.data);
-      return res.status(500).json('Error retrieving popular Yo\'s');
-    }
-  });
-
-  app.get('/api/recent', async (req, res) => {
-    const rec = await Yo.find({}).sort({"lastAccess": -1}).limit(10);
-    if(rec) {
-      return res.status(200).json(rec);
-    }else {
-      logger.error("Error retrieving recently used Yo\'s: " + res.data);
-      return res.status(500).json('Error retrieving recently used Yo\'s');
-    }
-  });
-
-  app.get('/api/stats', async (req, res) => {
-    var hits = 0;
-    const hits_data = await Yo.find({},{urlHits:1, _id:0}).sort({urlHits: -1});
-    for(i = 0; i < hits_data.length; i++) { 
-      if(hits_data[i].urlHits){
-        hits += hits_data[i].urlHits;
-      }
-    }
-    if(hits_data.length > 0) {
-      return res.status(200).json({
-        "totalYos": hits_data.length,
-        "totalHits": hits
-      });
-    }else {
-      logger.error("Error retrieving Yo stats: " + res.data);
-      return res.status(500).json('Error retrieving Yo stats');
-    }
-  });
-
-  app.get('/api/item/:name', YoCtrl.getCached)
-
-  app.post('/api/item', async (req, res) => {
-    const { shortBaseUrl, originalUrl, linkName } = req.body;
-    const ip = req.headers["x-real-ip"];
-    if(validUrl.isUri(shortBaseUrl)) {
-    }else {
-      logger.error("The Base URL provided in the config is not valid: " + shortBaseUrl);
-      return res.status(400).json('The Base URL provided in the config is not valid.');
-    }
-
-    const updatedAt = new Date();
-    const queryOptions = { linkName };
-    if(validUrl.isUri(originalUrl)) {
-      let urlData;
-      try {
-        /*
-        // Find the item in the cache
-        urlData = await cache.getFromCache('linkName', JSON.stringify(queryOptions));
-        */
-
-        if(!urlData) {
-          // Find if the item is in the db
-          urlData = await Yo.findOne(queryOptions).exec();
-        }
-
-        if(urlData) {
-          logger.info("User " + ip + " could not create a Yo as the name is already in-use: " + queryOptions.linkName);
-          res.status(401).json('This name is already in-use. Please select another name.');
-        } else {
-          shortUrl = shortBaseUrl + '/' + linkName;
-          const itemToBeSaved = { originalUrl, shortUrl, linkName, updatedAt };
-
-          // Add the item to db
-          const item = new Yo(itemToBeSaved);
-          await item.save();
-          // Add the item to cache
-          cache.addToCache('linkName', JSON.stringify(queryOptions), itemToBeSaved);
-          logger.info("User from " + ip + " created alias: " + linkName + " -> " + originalUrl);
-          res.status(200).json(itemToBeSaved);
-        }
-      }catch(err) {
-        logger.error("Invalid Session: " + err);
-        res.status(401).json('Invalid Session');
-      }
-    } else {
-      logger.warn("The provided URL is improperly formatted: " + originalUrl);
-      return res.status(400).json('The provided URL is improperly formatted.');
-    }
-  });
 };
