997b29b428c0ec06a65bc6a99e35306cb06264b3
Jon Fairbanks <fairplay89@gmail.com>
2019-06-16 17:47:47 -0700

    Endpoint Verification

--- client/src/APIHelper.js
+++ client/src/APIHelper.js
@@ -2,7 +2,10 @@ import axios from "axios";
 import constants from "./config/config";
 axios.defaults.baseURL = constants.apiUrl;
 
+var accessToken = localStorage.getItem("accessToken");
+const headers = { 'Content-Type': 'application/json', 'Authorization': "Bearer " + accessToken }
+
 export const createShortUrl = obj => {
   const requestUrl = "link";
-  return axios.post(requestUrl, obj);
+  return axios.post(requestUrl, obj, {headers: headers});
 };
--- server/routes/yo.js
+++ server/routes/yo.js
@@ -1,5 +1,16 @@
-const YoCtrl = require('../controllers/yo')
+const YoCtrl = require('../controllers/yo');
 const config = require('../config/config');
+const axios = require('axios');
+
+const authCheck = (req, res, next) => {
+  try{ var authToken = req.headers['authorization'] }catch(e) {res.status(401).json('Authentication Error')}
+  if (authToken && authToken.startsWith("Bearer "))
+    authToken = authToken.substring(7, authToken.length);
+  const headers = { 'Authorization': "Bearer " + authToken }
+  axios.get('https://' + config.auth0Domain + '/userinfo', {headers: headers})
+  .then((res) => { next() })
+  .catch((err) => { res.status(401).json('Authentication Error'); return })
+}
 
 module.exports = (app) => {
   app.get('/', (req, res) => {res.redirect(config.baseUrl)});
@@ -8,8 +19,12 @@ module.exports = (app) => {
   app.get('/api/latest', YoCtrl.getLatest) // Get latest Yos
   app.get('/api/popular', YoCtrl.getPopular) // Get popular Yos
   app.get('/api/recent', YoCtrl.getRecent) // Get recent Yos
-  app.get('/api/link/:name', YoCtrl.getYo, YoCtrl.emitSocketUpdate) // Get a single Yo and emit socket updates
-  app.post('/api/link', YoCtrl.postYo, YoCtrl.emitSocketUpdate) // Post a single Yo and emit socket updates
-  app.post('/api/update/:name', YoCtrl.updateYo, YoCtrl.emitSocketUpdate) // Update a single Yo
-  app.post('/api/delete/:name', YoCtrl.deleteYo, YoCtrl.emitSocketUpdate) // Delete a single Yo
+  app.get('/api/link/:name', YoCtrl.getYo, YoCtrl.emitSocketUpdate) // Redirect to a Yo
+  /*
+    Protected Paths
+  */
+  app.use(authCheck)
+  app.post('/api/link', YoCtrl.postYo, YoCtrl.emitSocketUpdate) // Create a new Yo
+  app.post('/api/update/:name', YoCtrl.updateYo, YoCtrl.emitSocketUpdate) // Update a Yo
+  app.post('/api/delete/:name', YoCtrl.deleteYo, YoCtrl.emitSocketUpdate) // Delete a Yo
 };
