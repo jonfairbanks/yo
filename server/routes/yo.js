255e1f5c6b9bd6a521a5f932417b85501b9013c3
Jon Fairbanks <fairplay89@gmail.com>
2020-09-27 12:50:54 -0700

    Fix Auth

--- server/routes/yo.js
+++ server/routes/yo.js
@@ -2,15 +2,21 @@ const axios = require('axios');
 const YoCtrl = require('../controllers/yo');
 
 const authCheck = (req, res, next) => {
-  try {
-    const headers = { Authorization: req.headers.authorization };
-    if (headers) {
-      axios.get(`https://${process.env.AUTH0_DOMAIN}/userinfo`, { headers })
-        .then(next())
-        .catch(res.status(401).json('Authentication Error'));
-    }
-  } catch (e) {
-    res.json(401).json('Authentication Error');
+  console.log(`Checking auth status @ ${Date.now()}`)
+  let headers = null;
+  if(process.env.AUTH === 'true' && req.headers && req.headers.authorization) {
+    headers = { Authorization: req.headers.authorization };
+  } else {
+    res.status(401).json('Authentication Error');
+  }
+
+  if(headers) {
+    axios.get(`https://${process.env.AUTH0_DOMAIN}/userinfo`, { headers })
+      .then(x => console.log(`Authentication successful as ${x.data.nickname}`))
+      .then(next())
+      .catch(() => {
+        res.status(401).json('Authentication Error')
+      });
   }
 };
 
@@ -19,7 +25,7 @@ module.exports = (app) => {
   app.get('/', (_req, res) => { res.redirect(process.env.BASE_URL); }); // Redirect lost users
   app.get('/api/link/:name', YoCtrl.getYo, YoCtrl.emitSocketUpdate); // Redirect to a Yo
   /* Protected Paths (if enabled) */
-  if (process.env.AUTH === 'true') {
+  if(process.env.AUTH === 'true') {
     console.log('\n** API Authentication Enabled **\n'); // eslint-disable-line
     app.use(authCheck);
   }
