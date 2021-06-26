e81a645096c72d7dbb64e1fcc0d8b1b72ea6dd18
bsord <brandon.sorgdrager@gmail.com>
2021-06-26 03:05:17 +0000

    Linter cleanup

--- client/src/APIHelper.js
+++ client/src/APIHelper.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = process.env.REACT_APP_API_URL ? process.env.REACT_APP_API_URL : window.REACT_APP_API_URL
+axios.defaults.baseURL = process.env.REACT_APP_API_URL ? process.env.REACT_APP_API_URL : window.REACT_APP_API_URL; //eslint-disable-line
 
 const accessToken = localStorage.getItem('accessToken'); // eslint-disable-line no-undef
 const headers = { 'Content-Type': 'application/json', Authorization: `Bearer ${accessToken}` };
