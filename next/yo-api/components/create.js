8afbcb0c44f5184f8df4eb5cae0b383f0d6e6051
Jon Fairbanks <fairplay89@gmail.com>
2024-10-23 23:33:33 -0700

    UI support to create new Yo links

--- next/yo-api/components/create.js
+++ next/yo-api/components/create.js
@@ -1,24 +1,65 @@
+import React, { useState } from 'react';
+
+const createNewYo = async (event) => {
+	event.preventDefault(); // Prevent page reload
+
+	const form = event.target;
+	const linkName = form.linkName.value;
+	const originalUrl = form.originalUrl.value;
+
+	const data = {
+    linkName: linkName,
+    originalUrl: originalUrl,
+  };
+
+  try {
+    const response = await fetch('/api/create', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
+
+    if (response.ok) {
+
+		} else {
+      throw new Error('Network response was not ok:', response);
+    }
+
+    const result = await response.json();
+    console.log('Success:', result);
+    form.reset(); // Reset form after successful submission
+  } catch (error) {
+    console.error('Error submitting form:', error);
+  }
+};
+
 const CreateModal = () => {
 	return (
-		<div id="create" className="modal">
-			<div className="modal-content">
-				<h4>Create a New Link</h4>
-				<div className="s12 m6 input-field">
-					<input id="link_name" type="text" placeholder="rick" maxlength="20" />
-					<label for="link_name">Link Name</label>
-					<span className="supporting-text">What should the new link be named?</span>
-					<br/><br/>
+		<form className="row" onSubmit={createNewYo}>
+			<div id="create" className="modal">
+				<div className="modal-content">
+					<h4>Create a New Link</h4>
 					<div className="s12 m6 input-field">
-						<input id="first_name" type="text" placeholder="https://www.youtube.com/watch?v=dQw4w9WgXcQ" />
-						<label for="first_name">Website URL</label>
-						<span className="supporting-text">What is the original url you want to redirect to?</span>
+						<input id="linkName" type="text" placeholder="rick" maxLength="120" />
+						<label htmlFor="linkName">Link Name</label>
+						<span className="supporting-text">What should the new link be named?</span>
+						<br /><br />
+						<div className="s12 m6 input-field">
+							<input id="originalUrl" type="text" placeholder="https://www.youtube.com/watch?v=dQw4w9WgXcQ" />
+							<label htmlFor="originalUrl">Website URL</label>
+							<span className="supporting-text">What is the original url you want to redirect users to?</span>
+						</div>
 					</div>
 				</div>
+				<div className="modal-footer">
+					<button type="submit" className="modal-close waves-effect btn-flat teal white-text">
+						Create
+					</button>
+				</div>
 			</div>
-			<div className="modal-footer">
-				<a href="#!" className="modal-close waves-effect btn-flat teal white-text">Create</a>
-			</div>
-		</div>
+		</form>
 	);
 };
 
