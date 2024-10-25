d3dd8aeb477cc9c383efa4cc0d6efdcb51daa04c
Jon Fairbanks <fairplay89@gmail.com>
2024-10-25 00:19:52 -0700

    Fix header CSS

--- next/yo-api/components/header.js
+++ next/yo-api/components/header.js
@@ -4,7 +4,7 @@ const Header = () => {
 	return (
 		<nav>
 			<div className="nav-wrapper">
-				<Image src="/images/apple-touch-icon.png" alt="Yo URL" className="header-logo" width={50} height={50} priority />
+				<Image src="/images/apple-touch-icon.png" alt="Yo URL" className="header-logo" width={45} height={45} priority />
 				<a href="#" className="brand grey-text">Yo</a>
 				<a href="#create" className="btn modal-trigger filled icon-left teal white-text text-darken-2">
 					<i className="material-icons">add</i>Create
