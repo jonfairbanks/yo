55b61f6d3fb30ccd7252dccf946fb112e57f9ca3
Jon Fairbanks <fairplay89@gmail.com>
2024-10-25 01:04:47 -0700

    Theme update

--- next/yo-api/components/header.js
+++ next/yo-api/components/header.js
@@ -6,7 +6,7 @@ const Header = () => {
 			<div className="nav-wrapper">
 				<Image src="/images/apple-touch-icon.png" alt="Yo URL" className="header-logo" width={45} height={45} priority />
 				<a href="#" className="brand grey-text">Yo</a>
-				<a href="#create" className="btn modal-trigger filled icon-left teal white-text text-darken-2">
+				<a href="#create" className="btn modal-trigger filled icon-left teal white-text text-darken-2 darken-2">
 					<i className="material-icons">add</i>Create
 				</a>
 			</div>
