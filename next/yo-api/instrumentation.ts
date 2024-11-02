072aa6023050af3dda385e9f6120caead848f5e6
Jon Fairbanks <fairplay89@gmail.com>
2024-11-02 00:36:31 -0700

    Otel Config

--- next/yo-api/instrumentation.ts
+++ next/yo-api/instrumentation.ts
@@ -1,5 +1,11 @@
 import { registerOTel } from '@vercel/otel'
 
 export function register() {
-    registerOTel({ serviceName: 'yo-api' })
+    console.log(`OTLP Export Endpoint: ${process.env.OTEL_EXPORTER_OTLP_ENDPOINT}`);
+    console.log(`OTLP Zipkin Endpoint: ${process.env.OTEL_EXPORTER_ZIPKIN_ENDPOINT}`);
+    console.log(`OTLP Jaeger Endpoint: ${process.env.OTEL_EXPORTER_JAEGER_ENDPOINT}`);
+
+    registerOTel({ 
+        serviceName: 'yo-api'
+    })
 }
