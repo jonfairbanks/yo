518aac9731644703934a6df2c5cf74a70078bde3
Jon Fairbanks <fairplay89@gmail.com>
2025-11-26 23:06:18 -0800

    Fix error

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -22,10 +22,6 @@ function MyApp({ Component, pageProps }) {
                     type="image/png"
                     href="/api/public/favicon.ico"
                 />
-                <link
-                    rel="stylesheet"
-                    href="https://fonts.googleapis.com/icon?family=Material+Icons"
-                />
             </Head>
             <Component {...pageProps} />
         </>
