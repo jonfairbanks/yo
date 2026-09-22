825de2fcb8f1105663bc0ccb6fc05fc78c61b969
Jon Fairbanks <fairplay89@gmail.com>
2026-09-22 00:24:36 -0700

    style: format ESLint configuration

--- src/eslint.config.mjs
+++ src/eslint.config.mjs
@@ -1,23 +1,23 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTypeScript from "eslint-config-next/typescript";
+import { defineConfig, globalIgnores } from 'eslint/config'
+import nextVitals from 'eslint-config-next/core-web-vitals'
+import nextTypeScript from 'eslint-config-next/typescript'
 
 export default defineConfig([
-  ...nextVitals,
-  ...nextTypeScript,
-  globalIgnores([
-    ".next/**",
-    "build/**",
-    "coverage/**",
-    "next-env.d.ts",
-    "out/**",
-    "public/vendor/**",
-  ]),
-  {
-    rules: {
-      "@typescript-eslint/no-unused-vars": "warn",
-      // This React Hooks 7 rule was not part of the prior lint configuration.
-      "react-hooks/set-state-in-effect": "off",
+    ...nextVitals,
+    ...nextTypeScript,
+    globalIgnores([
+        '.next/**',
+        'build/**',
+        'coverage/**',
+        'next-env.d.ts',
+        'out/**',
+        'public/vendor/**',
+    ]),
+    {
+        rules: {
+            '@typescript-eslint/no-unused-vars': 'warn',
+            // This React Hooks 7 rule was not part of the prior lint configuration.
+            'react-hooks/set-state-in-effect': 'off',
+        },
     },
-  },
-]);
+])
